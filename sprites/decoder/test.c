efa4c058fa6f8eb080d18a5e2ecd14c09d1a1954
Jonas ALLERHAND <jallerha@e3r9p3.clusters.42paris.fr>
2022-01-27 15:41:30 +0100

    show decoded sprite

--- sprites/decoder/test.c
+++ sprites/decoder/test.c
@@ -1,4 +1,28 @@
 #include "sprite.h"
+#include "mlx.h"
+#include <string.h>
+
+int color_to_hex(int index, t_palette palette)
+{
+    if (index == 0xFF)
+        return (-1);
+    int r = palette.colors[index].r;
+    int g = palette.colors[index].g;
+    int b = palette.colors[index].b;
+
+    return ((r << 16) | (g << 8) | b);
+}
+
+void ft_print_matrix(t_sprite sprite)
+{
+    for (int i = 0; i < sprite.height; i++)
+    {
+        printf("%d -> %2d | ", i, i * sprite.width);
+        for (int j = 0; j < sprite.width; j++)
+            printf("%2d ", sprite.pixels[i * sprite.width + j].palette_index);
+        puts("");
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -14,18 +38,30 @@ int main(int argc, char **argv)
 
     sprite = ft_new_sprite();
     ft_decode_sprite(fd, &sprite);
-
+    char *filename = strdup(argv[1]);
     puts("Sprite decoding ended.\n------------");
     printf("Compressed : %d\n", sprite.compressed);
     printf("Palette size : %d\n", sprite.palette.size);
     printf("Width: %d\n", sprite.width);
     printf("Height: %d\n", sprite.height);
     printf("Matrix:\n");
+    if (sprite.height * sprite.width > 1000)
+        printf("Too big to print -> %d\n", sprite.height * sprite.width);
+    else
+        ft_print_matrix(sprite);
+    void *mlx_ptr = mlx_init();
+    void *win_ptr = mlx_new_window(mlx_ptr, 300, 300, filename);
+
     for (int i = 0; i < sprite.height; i++)
     {
-        printf("%d -> %2d | ", i, i * sprite.width);
         for (int j = 0; j < sprite.width; j++)
-            printf("%2d ", sprite.pixels[i * sprite.width + j].palette_index);
-        puts("");
+        {
+            int color = sprite.pixels[i * sprite.width + j].palette_index;
+            int hex = color_to_hex(color, sprite.palette);
+            if (hex > 0)
+                mlx_pixel_put(mlx_ptr, win_ptr, j, i, hex);
+        }
     }
+    // show window
+    mlx_loop(mlx_ptr);
 }
