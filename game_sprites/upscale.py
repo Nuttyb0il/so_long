c9bb5cf4e7c30d87f3b514d853a449ea187ee705
Jonas ALLERHAND <jallerha@e1r5p4.42.fr>
2022-02-01 16:49:51 +0100

    changed upscale script for >1 scale

--- game_sprites/upscale.py
+++ game_sprites/upscale.py
@@ -7,7 +7,10 @@ args.add_argument("-s", "--scale", help="Scale factor", required=True)
 args = args.parse_args()
 
 image = Image.open(args.input)
-image = image.resize((int(image.width * float(args.scale)), int(image.height * float(args.scale))), Image.BOX)
+if float(args.scale) > 1:
+    image = image.resize((int(image.width * float(args.scale)), int(image.height * float(args.scale))), Image.BOX)
+else:
+    image = image.resize((int(image.width * float(args.scale)), int(image.height * float(args.scale))), Image.BICUBIC)
 
 filename, ext = args.input.split(".")
 image.save(filename + "_upscaled." + ext)
