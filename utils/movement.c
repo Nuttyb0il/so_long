72402b0b6e074cb899bbac68d8880e60fbf4179b
Jonas ALLERHAND <jallerha@e1r6p3.42.fr>
2022-02-02 16:46:44 +0100

    changed movement step, easier player movement

--- utils/movement.c
+++ utils/movement.c
@@ -2,18 +2,18 @@
 void ft_offset_position(int *x, int *y, int keycode)
 {
     if (keycode == 126)
-        *y -= 5;
+        *y -= 6;
     else if (keycode == 125)
-        *y += 5;
+        *y += 6;
     else if (keycode == 123)
-        *x -= 5;
+        *x -= 6;
     else if (keycode == 124)
-        *x += 5;
+        *x += 6;
 }
 
 void ft_move_player(t_game *game, int keycode)
 {
-    ft_offset_position(&game->map->player_x, &game->map->player_y, keycode);
-    ft_render_sprite(game, "madeline", game->map->player_x, game->map->player_y);
-    printf("Rendering player at %d, %d\n", game->map->player_x, game->map->player_y);
+    ft_offset_position(&game->map->player_pos.x, &game->map->player_pos.y, keycode);
+    ft_render_sprite(game, "madeline", game->map->player_pos.x, game->map->player_pos.y);
+    printf("Rendering player at %d, %d\n", game->map->player_pos.x, game->map->player_pos.y);
 }
