5b9f7dd2f7192af2901bbde27da3764218a46922
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-22 00:41:26 +0100

    removed leaks check for linux

--- errors/clean_exit.c
+++ errors/clean_exit.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 12:08:43 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/21 14:33:45 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/21 20:36:26 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	clean_exit(int code)
 {
 	ft_full_free();
 	ft_printf(INDIANRED1"\nLeaks :\n");
-	system("leaks -q so_long");
+	// system("leaks -q so_long");
 	ft_printf(RESET"\r");
 	exit(code);
 }
