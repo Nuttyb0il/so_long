1d4f24d113db2aaac1c4de8792b1bbceab8436cb
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-22 13:13:48 +0100

    improved game struct

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,12 +6,13 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 11:39:53 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/21 15:27:26 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/22 11:58:33 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+# include "../libft/includes/lists.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
@@ -28,22 +29,23 @@
 
 typedef struct s_map
 {
-	char	*map;
-	char	*path;
-	int		length;
-	int		height;
-	int		width;
-	int		sanity;
-	int		fd;
-	int		collectibles;
-	int		spawns;
-	int		exits;
+	char		*map;
+	char		*path;
+	t_chain_lst *lines;
+	int			length;
+	int			height;
+	int			width;
+	int			sanity;
+	int			fd;
+	int			collectibles;
+	int			spawns;
+	int			exits;
 }	t_map;
 
 t_map	ft_load_map(char *path);
 void	clean_exit(int code);
-int		ft_valid_line_length(char *s, int expected);
-int		ft_map_closed(char *s, int width);
+int		ft_valid_line_length(t_chain_lst *lines, int expected);
+int		ft_map_closed(t_chain_lst *lines);
 int		ft_valid_charset(char *s);
 void	ft_print_markdown(int code, t_map *map);
 int		ft_is_valid_char(char c);
