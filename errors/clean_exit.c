19ed5c14295f4d8ca8cd8c0afa0c8b9b6d563a67
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-24 00:49:05 +0100

    clean exit when mlx is initialized

--- errors/clean_exit.c
+++ errors/clean_exit.c
@@ -6,20 +6,46 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 12:08:43 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/21 20:36:26 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/23 13:40:21 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/memory.h"
 #include "../libft/includes/colors.h"
 #include "../libft/includes/printf.h"
+#include "../libft/includes/lists.h"
+#include "../includes/mlx.h"
+#include "../includes/so_long.h"
 #include <unistd.h>
+
+// clean exit but when MLX is initialized
+void	clean_exit_mlx(int code, t_game *game)
+{
+	int		i;
+
+	i = 0;
+	while (i < game->sprites_init)
+	{
+		if (game->sprites[i].image)
+			mlx_destroy_image(game->mlx_ptr, game->sprites[i].image);
+		i++;
+	}
+	if (game->mlx_ptr)
+		mlx_loop_end(game->mlx_ptr);
+	else
+		clean_exit(code);
+	if (game->game_window)
+		mlx_destroy_window(game->mlx_ptr, game->game_window);
+	mlx_destroy_display(game->mlx_ptr);
+	free(game->mlx_ptr);
+	clean_exit(code);
+}
 
 void	clean_exit(int code)
 {
 	ft_full_free();
-	ft_printf(INDIANRED1"\nLeaks :\n");
+	// ft_printf(INDIANRED1"\nLeaks :\n");
 	// system("leaks -q so_long");
-	ft_printf(RESET"\r");
+	// ft_printf(RESET"\r");
 	exit(code);
 }
