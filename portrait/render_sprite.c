3132379bae03c85c4a7ee16cbeaa912bccb89a16
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-24 00:47:09 +0100

    render a sprite by it's id (index in sprite table)

---
+++ portrait/render_sprite.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_sprite.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 19:17:57 by jallerha          #+#    #+#             */
+/*   Updated: 2022/03/23 11:57:24 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+#include "../includes/mlx.h"
+#include "../libft/includes/printf.h"
+#include "../libft/includes/colors.h"
+#include "../libft/includes/constants.h"
+#include "../libft/includes/lists.h"
+
+void	ft_render_sprite_by_id(t_game *game, int id, int x, int y)
+{
+	void		*image;
+
+	image = game->sprites[id].image;
+	if (image != NULL)
+		mlx_put_image_to_window(game->mlx_ptr, game->game_window, image, x, y);
+	else
+	{
+		ft_fprintf(STDERR,
+			RED2"Error : "INDIANRED2"Failed to render sprite %s#%d%s\n",
+			ORANGE2, id, RESET);
+		clean_exit_mlx(1, game);
+	}
+}
