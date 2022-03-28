ced8f037327470c6baa7d67f43ec2af099cc7000
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-28 02:30:28 +0200

    refactored main function

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 16:26:34 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/23 20:09:13 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/27 20:25:54 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,52 +18,44 @@
 #include "libft/includes/string.h"
 #include <stdlib.h>
 
-int	ft_compute_size(int	n)
+// TODO : print warning and show confirmation because map is too big
+// (negative size)
+void	ft_load_game_sprite(t_game *game, char *arg)
 {
-	return (n * 64);
-}
+	int	size;
 
-int redcross(t_game *game)
-{
-	clean_exit_mlx(0, game);
-	return (0);
-}
-
-int movement(int keycode, t_game *game)
-{
-	int	x;
-	int	y;
-
-	x = game->x;
-	y = game->y;
-	if (keycode == ESC)
-		clean_exit_mlx(0, game);
-	if (!ft_legal_move(keycode, game))
-		return (0);
-	if (keycode == UP)
-		game->y -= 64;
-	if (keycode == DOWN)
-		game->y += 64;
-	if (keycode == LEFT)
-		game->x -= 64;
-	if (keycode == RIGHT)
-		game->x += 64;
-	if (keycode == UP || keycode == DOWN || keycode == RIGHT || keycode == LEFT)
+	if (ft_strcmp(arg, "debug") == 0)
 	{
-		if (ft_get_tile_at(game, x / 64, y / 64) != 'E')
-			ft_render_sprite_by_id(game, 2, x, y);
-		else
-			ft_render_sprite_by_id(game, 1, x, y);
-		mlx_put_image_to_window(game->mlx_ptr, game->game_window,
-			game->player_image, game->x, game->y);
+		ft_load_sprite_table_file(game, "assets/debug_x64.tbl", 228132741);
+		return ;
 	}
-	return (0);
+	size = ft_table_size(game->width);
+	if (size == 64)
+		ft_load_sprite_table_file(game, "assets/x64.tbl", 2822933351);
+	else if (size == 32)
+		ft_load_sprite_table_file(game, "assets/x32.tbl", 2822933351);
+	else if (size == 48)
+		ft_load_sprite_table_file(game, "assets/x48.tbl", 2822933351);
+	else if (size < 0)
+		ft_load_sprite_table_file(game, "assets/x32.tbl", 2822933351);
 }
 
-int	animation(int keycode, t_game *game)
+void	ft_init_game(t_game *game, char *map, char *debug)
 {
-	// game->game_clock++;
-	return (0);
+	game->moves = 0;
+	game->game_clock = 0;
+	game->direction = DOWN;
+	game->map_data = ft_load_map(map);
+	game->mlx_ptr = mlx_init();
+	if (!game->mlx_ptr)
+		clean_exit_mlx(1, game);
+	game->game_window = mlx_new_window(game->mlx_ptr,
+			ft_compute_size(game->map_data.width),
+			ft_compute_size(game->map_data.height) + 24,
+			"so_long");
+	ft_load_game_sprite(game, debug);
+	game->player_image = game->sprites[7].image;
+	ft_render_map(game);
 }
 
 int	main(int argc, char **argv)
@@ -72,29 +64,22 @@ int	main(int argc, char **argv)
 	int		width;
 	int		height;
 
-	if (argc != 2)
-	{
-		ft_fprintf(STDERR,
-			PURPLE1"Usage: %s%s%s <map>\n%sExample :\n\t%s%s%s maps/map_01.ber%s",
-			HOTPINK4, argv[0], YELLOW, PURPLE1, HOTPINK4, argv[0], YELLOW, RESET
-			);
-		clean_exit(1);
-	}
+	// if (argc >= 2)
+	// {
+	// 	ft_fprintf(STDERR,
+	// 		PURPLE1"Usage: %s%s%s <map>\n%sExample :\n\t%s%s%s maps/map_01.ber%s",
+	// 		HOTPINK4, argv[0], YELLOW, PURPLE1, HOTPINK4, argv[0], YELLOW, RESET
+	// 		);
+	// 	clean_exit(1);
+	// }
 	ft_bzero(&game, sizeof(t_game));
-	game.moves = 0;
-	game.game_clock = 0;
-	game.map_data = ft_load_map(argv[1]);
-	game.mlx_ptr = mlx_init();
-	ft_load_sprites(&game);
-	game.game_window = mlx_new_window(game.mlx_ptr,
-		ft_compute_size(game.map_data.width),
-		ft_compute_size(game.map_data.height),
-		"so_long");
-	ft_render_map(&game);
-	game.player_image = game.sprites[7].image;
-	mlx_hook(game.game_window, 17, 0, redcross, &game);
-	mlx_hook(game.game_window, 2, 1, movement, &game);
-	mlx_loop_hook(game.mlx_ptr, animation, &game);
+	if (argc == 3)
+		ft_init_game(&game, argv[1], "debug");
+	else
+		ft_init_game(&game, argv[1], "");
+	mlx_hook(game.game_window, 17, 0, ft_redcross, &game);
+	mlx_hook(game.game_window, 2, 1, ft_key_hook, &game);
+	mlx_loop_hook(game.mlx_ptr, ft_loop_hook, &game);
 	mlx_loop(game.mlx_ptr);
 	clean_exit_mlx(0, &game);
 	return (0);
