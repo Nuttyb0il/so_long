31610fd2f8921adf1b1ceb68ad829d5c2929e87a
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-28 02:29:20 +0200

    refactor and using new sprite table format

--- portrait/render_map.c
+++ portrait/render_map.c
@@ -6,30 +6,33 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 19:44:52 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/23 17:06:19 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/27 16:59:04 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 #include "../includes/mlx.h"
 #include "../libft/includes/lists.h"
+#include "../libft/includes/memory.h"
 
 void	ft_render_tile(t_game *game, char tile, int x, int y)
 {
 	if (tile == '0')
-		ft_render_sprite_by_id(game, 2, x, y);
+		ft_render_sprite_by_id(game, 0, x, y);
 	else if (tile == '1')
-		ft_render_sprite_by_id(game, 20, x, y);
+		ft_render_sprite_by_id(game, 1, x, y);
 	else if (tile == 'P')
 	{
 		game->x = x;
 		game->y = y;
-		ft_render_sprite_by_id(game, 7, x, y);
+		ft_render_sprite_by_id(game, 6, x, y);
 	}
 	else if (tile == 'C')
-		ft_render_sprite_by_id(game, 19, x, y);
+	{
+		ft_render_sprite_by_id(game, 18, x, y);
+	}
 	else if (tile == 'E')
-		ft_render_sprite_by_id(game, 1, x, y);
+		ft_render_sprite_by_id(game, 28, x, y);
 }
 
 void	ft_render_map(t_game *game)
@@ -39,18 +42,22 @@ void	ft_render_map(t_game *game)
 	t_chain_lst	*line;
 	char		*str;
 
-	i = 0;
+	i = -1;
 	j = 0;
 	line = game->map_data.lines;
+	ft_init_animations(game);
 	while (line)
 	{
-		str = (char*)line->content;
-		while (str[i])
+		str = (char *) line->content;
+		while (str[++i])
 		{
+			if (str[i] == 'E')
+				ft_register_berry_animation(game, i * 64, j * 64);
+			if (str[i] == 'C')
+				ft_register_seed_animation(game, i * 64, j * 64);
 			ft_render_tile(game, str[i], i * 64, j * 64);
-			i++;
 		}
-		i = 0;
+		i = -1;
 		j++;
 		line = line->next;
 	}
