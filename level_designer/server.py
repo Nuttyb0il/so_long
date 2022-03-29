5c400244812c344cdb8b3daa5f699ac36dc31d51
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-29 18:00:31 +0200

    web level designer

---
+++ level_designer/server.py
@@ -0,0 +1,45 @@
+from bottle import Bottle, run, static_file, route, request
+from glob import glob
+import os
+import json
+
+app = Bottle()
+
+@app.route("/assets/<path>")
+def serve_utils(path):
+	return static_file(path, root="./static/assets")
+
+@app.route("/game_sprites/<name>")
+def serve_game_sprites(name):
+	return static_file(name, root="./static/assets/game_sprites")
+
+@app.route("/save", method="POST")
+def save_map_file():
+	name = request.forms.get("name")
+	data = request.forms.get("data")
+	output_path = os.path.join("../maps", name)
+	if os.path.exists(output_path):
+		return "Map already exists"
+	with open(output_path, "w") as f:
+		f.write(data)
+	return "Map saved to {}".format(output_path)
+
+@app.route("/levels")
+def levels():
+	paths = glob("../maps/*.ber")
+	levels = [os.path.basename(path) for path in paths]
+	return json.dumps(levels)
+
+@app.route("/load_level", method="POST")
+def load():
+	name = request.forms.get("name")
+	path = os.path.join("../maps", name)
+	with open(path, "r") as f:
+		data = f.read()
+	return data
+
+@app.route("/")
+def index():
+	return static_file("ui.html", root="./static")
+
+run(app, host="0.0.0.0", port=12834)
