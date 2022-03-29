d9a0f7538f1f695e9aa895fa94e58a9274fc48df
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-29 18:01:57 +0200

    temporary change to avoid cropped windows has no <64 sprites are available

--- utils/adaptive_size.c
+++ utils/adaptive_size.c
@@ -6,16 +6,16 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:17:08 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/27 17:00:08 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/29 14:44:15 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_compute_size(int width)
 {
-	if (width > 32 && width <= 48)
-		return (width * 48);
-	else if (width > 48 && width <= 64)
-		return (width * 32);
+	// if (width > 32 && width <= 48)
+	// 	return (width * 48);
+	// else if (width > 48 && width <= 64)
+	// 	return (width * 32);
 	return (width * 64);
 }
 
