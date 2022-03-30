19dc933529e020b287727685f8a60afb4fedda1e
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-30 15:54:58 +0200

    removed debug and better size computation

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 16:26:34 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/29 17:59:21 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:25:48 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,20 @@
 
 // TODO : print warning and show confirmation because map is too big
 // (negative size)
-void	ft_load_game_sprite(t_game *game, char *arg)
+void	ft_load_game_sprite(t_game *game)
 {
-	int	size;
-
-	if (ft_strcmp(arg, "debug") == 0)
-	{
-		ft_load_sprite_table_file(game, "assets/debug_x64.tbl", 228132741);
-		return ;
-	}
-	size = ft_table_size(game->width);
-	if (size == 64)
+	game->tile_size = ft_table_size(game->map_data.width);
+	if (game->tile_size == 64)
 		ft_load_sprite_table_file(game, "assets/x64.tbl", 2822933351);
-	else if (size == 32)
-		ft_load_sprite_table_file(game, "assets/x32.tbl", 2822933351);
-	else if (size == 48)
-		ft_load_sprite_table_file(game, "assets/x48.tbl", 2822933351);
-	else if (size < 0)
-		ft_load_sprite_table_file(game, "assets/x32.tbl", 2822933351);
+	else if (game->tile_size == 32)
+		ft_load_sprite_table_file(game, "assets/x32.tbl", 2166143880);
+	else if (game->tile_size == 48)
+		ft_load_sprite_table_file(game, "assets/x48.tbl", 968777608);
+	else
+		ft_load_sprite_table_file(game, "assets/x32.tbl", 2166143880);
 }
 
-void	ft_init_game(t_game *game, char *map, char *debug)
+void	ft_init_game(t_game *game, char *map)
 {
 	game->moves = 0;
 	game->game_clock = 0;
@@ -49,12 +42,13 @@ void	ft_init_game(t_game *game, char *map, char *debug)
 	game->mlx_ptr = mlx_init();
 	if (!game->mlx_ptr)
 		clean_exit_mlx(1, game);
+	ft_load_game_sprite(game);
 	game->game_window = mlx_new_window(game->mlx_ptr,
-			ft_compute_size(game->map_data.width),
-			ft_compute_size(game->map_data.height),
+			game->map_data.width * game->tile_size,
+			game->map_data.height * game->tile_size,
 			"so_long");
-	// TODO : Check if game_window is null
-	ft_load_game_sprite(game, debug);
+	if (!game->game_window)
+		clean_exit_mlx(1, game);
 	game->player_image = game->sprites[7].image;
 	ft_render_map(game);
 }
@@ -63,19 +57,16 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 
-	// if (argc >= 2)
-	// {
-	// 	ft_fprintf(STDERR,
-	// 		PURPLE1"Usage: %s%s%s <map>\n%sExample :\n\t%s%s%s maps/map_01.ber%s",
-	// 		HOTPINK4, argv[0], YELLOW, PURPLE1, HOTPINK4, argv[0], YELLOW, RESET
-	// 		);
-	// 	clean_exit(1);
-	// }
+	if (argc < 2)
+	{
+		ft_fprintf(STDERR,
+			PURPLE1"Usage: %s%s%s <map>\n%sExample :\n\t%s%s%s maps/map_01.ber%s",
+			HOTPINK4, argv[0], YELLOW, PURPLE1, HOTPINK4, argv[0], YELLOW, RESET
+			);
+		clean_exit(1);
+	}
 	ft_bzero(&game, sizeof(t_game));
-	if (argc == 3)
-		ft_init_game(&game, argv[1], "debug");
-	else
-		ft_init_game(&game, argv[1], "");
+	ft_init_game(&game, argv[1]);
 	mlx_hook(game.game_window, 17, 0, ft_redcross, &game);
 	mlx_hook(game.game_window, 2, 1, ft_key_hook, &game);
 	mlx_loop_hook(game.mlx_ptr, ft_loop_hook, &game);
