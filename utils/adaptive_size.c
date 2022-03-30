891d2420e51c95b965fbec332429c895109231b5
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-30 15:51:14 +0200

    fixed size calculation

--- utils/adaptive_size.c
+++ utils/adaptive_size.c
@@ -6,24 +6,15 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:17:08 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/29 14:44:15 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:30:48 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_compute_size(int width)
-{
-	// if (width > 32 && width <= 48)
-	// 	return (width * 48);
-	// else if (width > 48 && width <= 64)
-	// 	return (width * 32);
-	return (width * 64);
-}
-
 int	ft_table_size(int width)
 {
-	if (width > 32 && width <= 48)
+	if (width > 40 && width <= 53)
 		return (48);
-	else if (width > 48 && width <= 64)
+	else if (width > 48)
 		return (32);
 	return (64);
 }
