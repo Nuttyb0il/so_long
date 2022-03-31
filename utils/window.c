226fdcdd0dc9f9396281ec0154a23bb586f079ef
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-31 14:41:26 +0200

    exit with 0 when user refuses to load a map too big

--- utils/window.c
+++ utils/window.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 14:21:08 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/31 14:38:21 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/31 14:41:08 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_confirm(t_game *game)
 		ORANGE2"Are you sure you want to continue? (y/n) ", STDERR);
 	read(STDIN, choice, 1);
 	if (choice[0] == 'N' || choice[0] == 'n')
-		ft_clean_exit_mlx(1, game);
+		ft_clean_exit_mlx(0, game);
 }
 
 void	ft_create_window(t_game *game)
