6521b5e7f5ccadfeaa2ac15f77c301f66adb440f
Jonas ALLERHAND <jallerha@student.42.fr>
2022-04-07 12:32:14 +0200

    fixed adaptive size issue

--- utils/adaptive_size.c
+++ utils/adaptive_size.c
@@ -11,16 +11,17 @@
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+#include "../libft/includes/printf.h"
 
 int	ft_table_size(t_game *game)
 {
 	if (game->map_data.height <= 21 && game->map_data.width <= 41)
 		return (64);
-	else if (game->map_data.height >= 22 && game->map_data.height <= 28
-		&& game->map_data.width >= 42 && game->map_data.width <= 54)
+	else if ((game->map_data.height >= 22 && game->map_data.height <= 28)
+		|| (game->map_data.width >= 42 && game->map_data.width <= 54))
 		return (48);
-	else if (game->map_data.height > 28 && game->map_data.height <= 42
-		&& game->map_data.width >= 55 && game->map_data.width <= 81)
+	else if ((game->map_data.height > 28 && game->map_data.height <= 42)
+		|| (game->map_data.width >= 55 && game->map_data.width <= 81))
 		return (32);
 	else
 		return (-1);
